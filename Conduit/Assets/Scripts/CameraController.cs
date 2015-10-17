af8426a1b39083df5e7cf1531d0cc60769560883
waikayk <Kong_WaiKay@bah.com>
2015-10-17 03:30:59 -0400

    Player tweaks. Organization changes. Code refinement. Lighting and stuff. Basic controls.

---
+++ Conduit/Assets/Scripts/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	public Camera camObject;
+	public float zoomMin = 5f;
+	public float zoomMax = 30f;
+	public float zoomSensitivity = 1f;
+	
+	public Transform followThis;
+	public float smoothTime = 0.3f;
+    private Vector3 velocity = Vector3.zero;
+	
+	void Start(){
+		//Clamp to min/max
+		Zoom(0);
+	}
+	
+	void Update(){
+		Follow();
+		ProcessInput();
+	}
+	
+	void Follow(){
+		if(!followThis) return;
+		Vector3 targetPosition = followThis.position;//.TransformPoint(new Vector3(0, 5, -10));
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+	}
+	
+	void ProcessInput(){
+		float mouseWheel = Input.GetAxis("Mouse ScrollWheel");
+		if(mouseWheel != 0){
+			Zoom(mouseWheel);
+		}
+	}
+	
+	void Zoom(float input){
+		float zoomHeight = camObject.transform.localPosition.y - (input * zoomSensitivity);
+		zoomHeight = Mathf.Clamp(zoomHeight, zoomMin, zoomMax);
+		camObject.transform.localPosition = new Vector3(0, zoomHeight, 0);
+	}
+}
--- Conduit/Assets/Scripts/PlayerController.cs
+++ Conduit/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-	
+	public float slowSpeed = 5f;
 	public float speed = 15f;
 	public float lookSpeed = 7f;
 	
@@ -12,22 +12,35 @@ public class PlayerController : MonoBehaviour {
 		playerPhysics = GetComponent<Rigidbody>();
 	}
 	
-	void FixedUpdate () {
+	void Update () {
 		ProcessPlayerInput();
 	}
 	
 	void ProcessPlayerInput(){
 		//Apply Player Axis Input
 		Vector3 inputAxis = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+		bool isShifted = Input.GetButton("Boost");
 		//Vector3 moveAxis = transform.right * inputAxis.x + transform.forward * inputAxis.z;
 		if(inputAxis != Vector3.zero){
-			playerPhysics.MovePosition(transform.position + inputAxis * Time.fixedDeltaTime * speed);
 			playerPhysics.MoveRotation(Quaternion.LookRotation(inputAxis, Vector3.up));
+			playerPhysics.MovePosition(transform.position + inputAxis * Time.deltaTime * (isShifted ? slowSpeed: speed));
 		}
-		
-//		Vector3 mouseAxis = new Vector3(Input.GetAxis("Mouse X"), 0, Input.GetAxis("Mouse Y"));
-//		if(mouseAxis != Vector3.zero){
-//			playerPhysics.MoveRotation(transform.rotation * Quaternion.AngleAxis(mouseAxis.x * lookSpeed, Vector3.up));
-//		}
+	}
+	
+	void OnCollisionEnter(Collision other){
+		OnCollisionStay(other);
+	}
+	
+	void OnCollisionStay(Collision other){
+		//For smooth looking movements
+		if(other.transform.tag == "Block" || other.transform.tag == "Immovable"){
+			playerPhysics.interpolation = RigidbodyInterpolation.None;
+		}
+	}
+	
+	void OnCollisionExit(Collision other){
+		if(other.transform.tag == "Block" || other.transform.tag == "Immovable"){
+			playerPhysics.interpolation = RigidbodyInterpolation.Interpolate;
+		}
 	}
 }
